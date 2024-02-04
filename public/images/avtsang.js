53159966a13a717f1d3e2268cffe09cc8a9e66b8
anhladth2007 <128598265+anhladth2007@users.noreply.github.com>
2024-02-04 15:44:21 +0700

    Update avtsang.js

--- public/images/avtsang.js
+++ public/images/avtsang.js
@@ -1,1 +1,16 @@
-
+exports.name = '/images/avtsang';
+exports.index = async(req, res, next) => {
+    try {
+        const a = require('./data/json/avtsang.json');
+        var image = a[Math.floor(Math.random() * a.length)].trim();
+        res.jsonp({
+            url: image,
+            data: image,
+            count: a.length,
+            author: 'dthdev',
+            status: true
+        });
+    } catch (e) {
+        return res.jsonp({ error: e });
+    }
+}
